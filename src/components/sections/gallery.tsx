b7bb7e3452caf69e704e00aaf6ff31923d8bf5f9
karthikeyanp.24mca <karthikeyanp.24mca@kongu.edu>
2025-11-26 21:53:16 +0000

    G

--- src/components/sections/gallery.tsx
+++ src/components/sections/gallery.tsx
@@ -13,6 +13,10 @@ const GallerySection = () => {
             <img src="https://images.unsplash.com/photo-1631515243349-e0cb75fb8d3a?w=800&q=80" alt="North Indian Thali" className="h-48 rounded-lg" />
             <img src="https://images.unsplash.com/photo-1585937421612-70a008356fbe?w=800&q=80" alt="Wedding Catering" className="h-48 rounded-lg" />
             <img src="https://images.unsplash.com/photo-1546833999-b9f581a1996d?w=800&q=80" alt="Function Catering" className="h-48 rounded-lg" />
+            <img src="https://images.unsplash.com/photo-1626777552726-4a6b54c97e47?w=800&q=80" alt="Dosa" className="h-48 rounded-lg" />
+            <img src="https://images.unsplash.com/photo-1604329760661-e71dc83f8426?w=800&q=80" alt="Samosas" className="h-48 rounded-lg" />
+            <img src="https://images.unsplash.com/photo-1589647363585-f4a7d38779df?w=800&q=80" alt="Biryani" className="h-48 rounded-lg" />
+            <img src="https://images.unsplash.com/photo-1628585352247-c46b5e080763?w=800&q=80" alt="Vada" className="h-48 rounded-lg" />
           </div>
         </marquee>
       </div>
