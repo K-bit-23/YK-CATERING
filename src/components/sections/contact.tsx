a348c3b2e3d129c343d4908b900b3c9c587b6d92
Karthikeyan P 24MCR051 <karthikeyanp.24mca@kongu.edu>
2025-11-26 19:46:29 +0000

    K.Y. Catering Online Menu Reviews Contact Toggle language Get a Quote A

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useFormState, useFormStatus } from 'react-dom';
+import { useEffect, useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { submitContactForm, type ContactState } from '@/app/actions';
 import { useToast } from '@/hooks/use-toast';
 import { Button } from '@/components/ui/button';
@@ -27,7 +27,7 @@ function SubmitButton() {
 export default function ContactSection() {
   const { t } = useI18n();
   const initialState: ContactState = { message: null, errors: {}, success: false };
-  const [state, dispatch] = useFormState(submitContactForm, initialState);
+  const [state, dispatch] = useActionState(submitContactForm, initialState);
   const { toast } = useToast();
 
   useEffect(() => {
